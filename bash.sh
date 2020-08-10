06c2fb8ec35e66139464e2f7ba55121bd1874c3e
TheCityEmpty <lxhaweb@163.com>
2020-08-10 14:22:18 +0800

    冲突测试

--- bash.sh
+++ bash.sh
@@ -3,10 +3,15 @@ git status
 
 echo '将所有变动文件添加到缓存区->'
 git add .
+echo '将所有变动文件添加到缓存区->' + $?
 
 echo '请输入commit消息->'
 read commitMsg
 git commit -m $commitMsg
+echo '请输入commit消息->' + $?
 
 echo '将代码推入到远程分支->'
 git push
+
+echo '将代码推入到远程分支息->' + $?
+

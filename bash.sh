d2c54900550e365d8013f16bd98d6f48dba220cc
TheCityEmpty <lxhaweb@163.com>
2020-08-10 10:32:39 +0800

    执行bash命令

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo '检查变动文件->'
+git status
+
+echo '将所有变动文件添加到缓存区->'
+git add .
+
+echo '请输入commit消息->'
+read commitMsg
+git commit -m $commitMsg
+
+echo '将代码推入到远程分支->'
+git push

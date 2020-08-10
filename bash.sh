f87179c9a46bd5b476ceb26107264ffb2ab5a30a
TheCityEmpty <lxhaweb@163.com>
2020-08-10 15:17:04 +0800

    重新测试冲突

--- bash.sh
+++ bash.sh
@@ -16,11 +16,11 @@ if [ $1 ] ; then
   fi
 
 git push
-pushRes = "$?"
-# echo '将代码推入到远程分支息->' + $pushRes
-echo $pushRes
-# if [ $pushRes = '1' ] ; then
-#   git pull
-#   echo '代码有冲突， 请在本地修改，修改完继续执行此条命令 bash bash.sh'
-# fi
+pushRes=$?
+echo '将代码推入到远程分支息->' + $pushRes
+
+if [ $pushRes = '1' ] ; then
+  git pull
+  echo '代码有冲突， 请在本地修改，修改完继续执行此条命令 bash bash.sh'
+fi
 

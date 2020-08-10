ad35101ce5a4015e63003ede9bb4fce135c13aa0
TheCityEmpty <lxhaweb@163.com>
2020-08-10 14:50:11 +0800

    冲突测试3

--- bash.sh
+++ bash.sh
@@ -1,19 +1,27 @@
+#!/usr/bin/env sh
+
+set -u
+
 git status
 echo '检查变动文件->' + $?
 
 git add .
 echo '将所有变动文件添加到缓存区->' + $?
-echo $1
 
 if [ $1 ] ; then
-    echo 'a'
     git commit -m $1
   else
-    echo 'b'
     read commitMsg
     git commit -m $commitMsg
   fi
 
 git push
 echo '将代码推入到远程分支息->' + $?
+
+if [ $? = 1 ] ; then
+  git pull
+  if [ $? = 1 ] ; then
+    git push
+  fi
+fi
 
